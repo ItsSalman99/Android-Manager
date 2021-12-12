ecc2bd3313eef1139f6e0273c9adfa72940267a6
Salman Abbas <salmanabbas985@gmail.com>
2021-12-12 14:55:16 +0300

    MainNavigation file added

---
+++ src/navigations/MainNavigation.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+
+const MainNavigation = () => {
+    return(
+        <NavigationContainer>
+        </NavigationContainer>
+    )
+}
+
+export default MainNavigation
