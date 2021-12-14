f6b74f860d0b1139cf070b1cae7e78e4d1ca9f32
Salman Abbas <salmanabbas985@gmail.com>
2021-12-14 20:43:39 +0300

    SimpleProgress updated

--- src/components/SimpleProgress.js
+++ src/components/SimpleProgress.js
@@ -2,23 +2,24 @@ import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View, Text } from 'react-native';
 import ProgressCircle from 'react-native-progress-circle'
 
-const SimpleProgress = ({percent,color,bgcolor}) => {
+const SimpleProgress = ({currentstor,memory,color,bgcolor}) => {
+  
   return (
     <ProgressCircle
-            percent={30}
-            radius={85}
-            borderWidth={12}
-            color="#16A085"
-            shadowColor="#85C1E9"
+            percent={currentstor}
+            radius={100}
+            borderWidth={10}
+            color="#5599fc"
+            shadowColor="#e5edfa"
             bgColor="#fff"
         >
             {/* <Text style={{ fontSize: 24 }}>{'30%'}</Text> */}
             <ProgressCircle
-            percent={80}
-            radius={70}
-            borderWidth={16}
-            color="#1F618D"
-            shadowColor="#85C1E9"
+            percent={memory}
+            radius={90}
+            borderWidth={20}
+            color="#98dffd"
+            shadowColor="#e5edfa"
             bgColor="#fff"
             ></ProgressCircle>
         </ProgressCircle>
