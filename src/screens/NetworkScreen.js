e5d2e2aab7acfc4b08444d99aaa1c97718232fa7
Salman Abbas <salmanabbas985@gmail.com>
2021-12-14 20:42:35 +0300

    NetworkScreen.js added

---
+++ src/screens/NetworkScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+const NetworkScreen = () => {
+    return (
+        <View style={{backgroundColor: '#fff', flex: 1, flexDirection: 'column', padding: 10}}>
+
+        </View>
+    )
+}
+
+export default NetworkScreen;
