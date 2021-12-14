aced7dc4bb1d30b6fd43d2e20117666603f8352c
Salman Abbas <salmanabbas985@gmail.com>
2021-12-14 20:38:00 +0300

    MainNavigation.js updated

--- src/navigations/MainNavigation.js
+++ src/navigations/MainNavigation.js
@@ -1,11 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
+import NetworkScreen from '../screens/NetworkScreen';
+import PhoneDetails from '../screens/PhoneDetails';
 import BottomTabs from './BottomTabs'
+
+const Stack = createNativeStackNavigator();
 
 const MainNavigation = () => {
     return(
         <NavigationContainer>
-            <BottomTabs/>
+            <Stack.Navigator initialRouteName='Android Manager'>
+                <Stack.Screen name="Android Manager" component={BottomTabs}/>
+                <Stack.Screen name="Phone Details" component={PhoneDetails}/>
+                <Stack.Screen name="Network Details" component={NetworkScreen}/>
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
