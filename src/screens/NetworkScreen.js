464c889dab1eecc768eaf11b3470f0b4443746fe
Salman Abbas <salmanabbas985@gmail.com>
2021-12-15 23:02:37 +0300

    NetworkScreen updated

--- src/screens/NetworkScreen.js
+++ src/screens/NetworkScreen.js
@@ -1,11 +1,49 @@
-import React from 'react'
-import {View,Text} from 'react-native'
-
+import React, {useEffect, useState} from 'react'
+import {View,Text, ScrollView, TouchableOpacity} from 'react-native'
+import DeviceInfo from 'react-native-device-info'
 const NetworkScreen = () => {
-    return (
-        <View style={{backgroundColor: '#fff', flex: 1, flexDirection: 'column', padding: 10}}>
 
+    const [Sim, setSim] = useState('')
+    const [MacAddress, setMacAddress] = useState('')
+    const [IpAddress, setIpAddress] = useState('')
+    useEffect(() => {
+        DeviceInfo.getCarrier().then((carrier) => {
+            setSim(carrier);
+          });
+        DeviceInfo.getMacAddress().then((mac) => {
+            setMacAddress(mac);
+          });
+          DeviceInfo.getIpAddress().then((ip) => {
+            setIpAddress(ip);
+          });
+        return () => {
+        }
+    })
+
+    return (
+        <ScrollView>
+        <View style={{backgroundColor: '#fff', flex: 1, flexDirection: 'column', padding: 10}}>
+            <Text style={{color: '#000', fontSize: 20, fontWeight: 'bold', marginVertical: 10}}>
+                MORE ABOUT YOUR DEVICE:
+            </Text>
+            <TouchableOpacity>
+            <Text style={{color: '#000',borderRadius: 20, borderWidth: 0.1, padding: 10, marginBottom: 10}}>
+                Network Operator : {Sim}
+            </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <Text style={{color: '#000',borderRadius: 20, borderWidth: 0.1, padding: 10, marginBottom: 10}}>
+                Mac Address: {MacAddress}
+            </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <Text style={{color: '#000',borderRadius: 20, borderWidth: 0.1, padding: 10, marginBottom: 10}}>
+                Mac Address: {IpAddress}
+            </Text>
+            </TouchableOpacity>
+            
         </View>
+        </ScrollView>
     )
 }
 
