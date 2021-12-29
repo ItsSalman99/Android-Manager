b55c019d351c52f50134e0b7062bb9fc21ffb866
Salman Abbas <salmanabbas985@gmail.com>
2021-12-29 23:07:13 +0300

    BottomTab updated

--- src/navigations/BottomTabs.js
+++ src/navigations/BottomTabs.js
@@ -3,9 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from '../screens/HomeScreen';
 import Fontawesome from 'react-native-vector-icons/FontAwesome'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
-import ToolBox from '../screens/ToolBoxScreen';
-import DiscoverScreen from '../screens/DiscoverScreen';
 import MeScreen from '../screens/MeScreen';
+import NetworkScreen from '../screens/NetworkScreen';
+import CameraScreen from '../screens/CameraScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +13,7 @@ const BottomTabs = () => {
     return(
         <Tab.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
             <Tab.Screen name='Home' component={Home} options={{
+                headerShown: true,
                 tabBarStyle:{ height: 60, paddingBottom: 5,},
                 headerTitle: 'Android Manager',
                 tabBarLabelStyle:{
@@ -23,34 +24,34 @@ const BottomTabs = () => {
                     return (<Fontawesome name='home' size={25} color="dodgerblue" />)
                 }
             }}/>
-            <Tab.Screen name='Toolbox' component={ToolBox} options={{
+            <Tab.Screen name='Network' component={NetworkScreen} options={{
                 tabBarStyle:{ height: 60, paddingBottom: 5},
                 tabBarLabelStyle:{
                     fontSize: 12,
                     fontFamily: 'helvetica'
                 },
                 tabBarIcon: () => {
-                    return (<MaterialIcon name='handyman' size={25} color="dodgerblue" />)
+                    return (<MaterialIcon name='wifi' size={25} color="dodgerblue" />)
                 }
             }}/>
-            <Tab.Screen name='Discover' component={DiscoverScreen} options={{
+            <Tab.Screen name='Camera' component={CameraScreen} options={{
                 tabBarStyle:{ height: 60, paddingBottom: 5},
                 tabBarLabelStyle:{
                     fontSize: 12,
                     fontFamily: 'helvetica'
                 },
                 tabBarIcon: () => {
-                    return (<MaterialIcon name='swipe' size={25} color="dodgerblue" />)
+                    return (<MaterialIcon name='camera' size={25} color="dodgerblue" />)
                 }
             }}/>
-            <Tab.Screen name='Me' component={MeScreen} options={{
+            <Tab.Screen name='Files' component={MeScreen} options={{
                 tabBarStyle:{ height: 60, paddingBottom: 5},
                 tabBarLabelStyle:{
                     fontSize: 12,
                     fontFamily: 'helvetica'
                 },
                 tabBarIcon: () => {
-                    return (<MaterialIcon name='people' size={25} color="dodgerblue" />)
+                    return (<MaterialIcon name='file-copy' size={25} color="dodgerblue" />)
                 }
             }}/>
         </Tab.Navigator>
